062699aa5e1f4650f549a0b3e6f69df914df0b36
ibrahimcanerdogan <i.erdogan@ebebek.com>
2023-11-05 23:38:58 +0300

    Object-Oriented Design finished.

---
+++ 1 -  Object-Oriented Design/Week 4/Capstone Assignment 1.4 – Update the Application - Solution/app/src/main/java/com/example/sharingapp/Contact.java
@@ -0,0 +1,54 @@
+package com.example.sharingapp;
+
+import java.util.UUID;
+
+public class Contact {
+
+    private String username;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    private String id;
+
+
+
+    public Contact(String username, String email, String id) {
+        this.username = username;
+        this.email = email;
+
+        if (id == null){
+            setId();
+        } else {
+            updateId(id);
+        }
+    }
+
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public void updateId(String id){
+        this.id = id;
+    }
+
+
+}
