02b8e75c35d3545e2e1a2d7f3388c5fd0c7678c6
ibrahimcanerdogan <i.erdogan@ebebek.com>
2023-11-07 18:05:00 +0300

    Design Patterns finished.

---
+++ 2 - Design Patterns/Week 4/Capstone Assignment 2.3 – Identify and Fix Code Smells - Ibrahim Can Erdogan/AddContactActivity.java
@@ -0,0 +1,65 @@
+package com.ibrahimcanerdogan.sharingapp;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    private ContactList contact_list = new ContactList();
+    private ContactListController contact_list_controller = new ContactListController(contact_list);
+
+    private Context context;
+
+    private EditText username;
+    private EditText email;
+
+    private String username_str;
+    private String email_str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+
+        username = (EditText) findViewById(R.id.username);
+        email = (EditText) findViewById(R.id.email);
+
+        context = getApplicationContext();
+        contact_list_controller.loadContacts(context);
+    }
+
+    public void saveContact(View view) {
+
+        username_str = username.getText().toString();
+        email_str = email.getText().toString();
+
+        if(!validateInput()) return;
+
+        Contact contact = new Contact(username_str, email_str, null);
+
+        // Add contact
+        boolean success = contact_list_controller.addContact(contact, context);
+        if (!success) {
+            return;
+        }
+
+        // End AddContactActivity
+        finish();
+    }
+
+    public boolean validateInput() {
+        if (username_str.equals("")) {
+            username.setError("Empty field!");
+            return false;
+        }
+
+        if (!email_str.contains("@")) {
+            email.setError("Must be an email address!");
+            return false;
+        }
+
+        if (!contact_list_controller.isUsernameAvailable(username_str)){
+            username.setError("Username already taken!");
+            return false;
+        }
+
+        return true;
+    }
+}
